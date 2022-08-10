433d18507f126e73da5c8f88e76764663cafac9e
DongHyun Kim <developerforus@gmail.com>
2022-08-10 17:27:59 +0900

    db connection succ

--- root/index.ts
+++ root/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
-
+import { AppDataSource } from "./db/data-source";
 const app = express();
 
-app.listen(3000, () => {
-  console.log("3000번 포트 온");
-});
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(3000, () => {
+      console.log("3000번 포트 온");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
