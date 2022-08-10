f0e9d7c32e364e51511f1183ba0cee8f03240fea
DongHyun Kim <developerforus@gmail.com>
2022-08-10 19:39:34 +0900

    getrepository version

--- root/src/db/models/user.ts
+++ root/src/db/models/user.ts
@@ -3,11 +3,12 @@ import { AppDataSource } from "../data-source";
 
 export class UserModel {
   public async create(email: string, password: string, name: string) {
-    const user = new User();
-    user.email = email;
-    user.name = name;
-    user.password = password;
-    await AppDataSource.manager.save(user);
-    return user;
+    const user = await AppDataSource.getRepository(User).create({
+      email,
+      password,
+      name,
+    });
+    const results = await AppDataSource.getRepository(User).save(user);
+    return results;
   }
 }
