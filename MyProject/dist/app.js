dd36f7f06c9b179aa60a4cd769500ef34b4b2af4
DongHyun Kim <developerforus@gmail.com>
2022-08-12 16:47:50 +0900

    morgan add

--- MyProject/dist/app.js
+++ MyProject/dist/app.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const api_1 = require("./api");
+const morgan_1 = __importDefault(require("morgan"));
 const errorMiddleware_1 = require("./api/middlewares/errorMiddleware");
 const app = (0, express_1.default)();
 exports.app = app;
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
+app.use((0, morgan_1.default)("dev"));
 app.use(api_1.router);
 app.use(errorMiddleware_1.errorMiddleware);
 //# sourceMappingURL=app.js.map
--- MyProject/src/app.ts
+++ MyProject/src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 
 import { router } from "./api";
-
+import morgan from "morgan";
 import { errorMiddleware } from "./api/middlewares/errorMiddleware";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use(router);
 
 app.use(errorMiddleware);
