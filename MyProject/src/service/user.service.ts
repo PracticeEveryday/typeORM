bb58e8c50dc757bf527d01c7559d6fb56de2b36c
DongHyun Kim <developerforus@gmail.com>
2022-08-13 21:25:03 +0900

    user login

--- MyProject/src/service/user.service.ts
+++ MyProject/src/service/user.service.ts
@@ -11,12 +11,15 @@ export class UserService {
     this.userRepository = MyDataSource.getRepository("user");
   }
 
-  public create = async (user: UserEntity) => {
+  public create = async (user: UserEntity): Promise<UserEntity> => {
     const newUser = await this.userRepository.save(user);
     return newUser;
   };
 
-  public login = async (email: string, password: string) => {
+  public login = async (
+    email: string,
+    password: string
+  ): Promise<UserEntity> => {
     const user = await this.userRepository.findOne({
       where: {
         email: email,
@@ -25,11 +28,12 @@ export class UserService {
 
     if (!user) {
       // 가입 이메일 없음.
-      const error = new HttpException(
-        404,
-        "해당 이메일로 가입한 유저가 없습니다."
-      );
-      throw error;
+      throw new HttpException(404, "해당 이메일로 가입한 유저가 없습니다.");
+    } else if (user.password === password) {
+      // 로그인
+    } else {
+      // 비밀번호 다름
+      throw new HttpException(400, "비밀번호가 틀립니다.");
     }
     return user;
   };
