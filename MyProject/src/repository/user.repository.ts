2a6d55058e5cbc780beb6f875beaf90b842717af
DongHyun Kim <developerforus@gmail.com>
2022-08-13 18:18:35 +0900

    class => interface

--- MyProject/src/repository/user.repository.ts
+++ MyProject/src/repository/user.repository.ts
@@ -3,4 +3,4 @@ import { MyDataSource } from "../db/data-source";
 import { Repository } from "typeorm";
 import { UserEntity } from "../db/entity/user.entity";
 
-export class UserRepository extends Repository<UserEntity> {}
+export interface UserRepository extends Repository<UserEntity> {}
