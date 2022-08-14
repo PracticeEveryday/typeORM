a2eb37114d263a32f5b81701d8707da47e24c311
DongHyun Kim <developerforus@gmail.com>
2022-08-14 14:55:27 +0900

    swagger apply

--- MyProject/src/app.ts
+++ MyProject/src/app.ts
@@ -5,14 +5,21 @@ import { UserController } from "./controller/user.controller";
 import morgan from "morgan";
 import { errorMiddleware } from "./setting/middlewares/errorMiddleware";
 
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+
 import dotenv from "./config";
 
 class Server {
   public app: express.Application;
   private userController: UserController;
+  private swaggerSpec;
+
   constructor() {
     const app: express.Application = express();
     this.app = app;
+    this.swaggerSpec = YAML.load(path.join(__dirname, "../swagger.yaml"));
   }
 
   private setRouter() {
@@ -24,6 +31,12 @@ class Server {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(morgan("dev"));
+    // swagger
+    this.app.use(
+      "/swagger",
+      swaggerUi.serve,
+      swaggerUi.setup(this.swaggerSpec)
+    );
 
     this.setRouter();
     this.app.use(errorMiddleware);
